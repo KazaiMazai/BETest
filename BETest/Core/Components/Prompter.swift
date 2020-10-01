73ca498cbf16ce87ed0359d432d8a767c5af0eb5
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-01 21:08:07 +0300

    implemented prompter core component

---
+++ BETest/Core/Components/Prompter.swift
@@ -0,0 +1,93 @@
+//
+//  Reader.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 01.10.2020.
+//
+
+import Foundation
+
+struct Prompter {
+    let delay: TimeInterval
+
+    private var pendingItems: [TextData] = []
+    private var state: State = .none
+
+    private(set) var items: [TextData] = []
+
+    init(delay: TimeInterval) {
+        self.delay = delay
+    }
+
+    mutating func reduce(_ action: Action) {
+        switch action {
+        case is Actions.PrompterFlow.Run:
+            state = .waitingForData
+        case let action as Actions.TextDataSource.ReceievedDataSuccess:
+            pendingItems.append(contentsOf: action.value)
+            guard waitingForData && !pendingItems.isEmpty else {
+                break
+            }
+
+            let item = pendingItems.removeFirst()
+            state = .processingItem(item, speechAvailableAfter: Date().addingTimeInterval(delay * 2.0))
+            items.append(item)
+        case let action as Actions.SpeechSynthesizer.StateChange:
+            guard case .finish = action.state else {
+                break
+            }
+
+            guard !pendingItems.isEmpty else {
+                break
+            }
+
+            let item = pendingItems.removeFirst()
+            state = .pendingItem(item, availableAfter: Date().addingTimeInterval(delay))
+        case let action as Actions.Time.TimeChanged:
+            guard case let .pendingItem(item, availableAfter) = state else {
+                break
+            }
+
+            guard availableAfter <= action.timestamp else {
+                break
+            }
+
+            state = .processingItem(item, speechAvailableAfter: Date().addingTimeInterval(delay * 2.0))
+            items.append(item)
+        default:
+            break
+        }
+    }
+}
+
+extension Prompter {
+    enum State {
+        case none
+        case waitingForData
+        case pendingItem(TextData, availableAfter: Date)
+        case processingItem(TextData, speechAvailableAfter: Date)
+        case finished
+    }
+}
+
+extension Prompter {
+    var waitingForData: Bool {
+        guard case .waitingForData = state else {
+            return false
+        }
+
+        return true
+    }
+
+    func availableForSpeech(at: Date) -> TextData? {
+        guard case let .processingItem(item, speechAvailableAfter) = state else {
+            return nil
+        }
+
+        guard speechAvailableAfter <= at else {
+            return nil
+        }
+
+        return item
+    }
+}
