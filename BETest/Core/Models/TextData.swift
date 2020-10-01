3ef6080e7d3e900df4d3df224bc16e04a6958e58
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-01 14:24:10 +0300

    added text data model

---
+++ BETest/Core/Models/TextData.swift
@@ -0,0 +1,13 @@
+//
+//  TextData.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 01.10.2020.
+//
+
+import Foundation
+
+struct TextData: Codable {
+    let id: Int
+    let text: String
+}
