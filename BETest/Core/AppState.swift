a6eaa1ed011791ceeb2291dbfeab9420e3ae2dfc
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-01 14:07:00 +0300

    added empty app state

---
+++ BETest/Core/AppState.swift
@@ -0,0 +1,14 @@
+//
+//  AppState.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 01.10.2020.
+//
+
+import Foundation
+
+struct AppState: Codable {
+    mutating func reduce(_ action: Action) {
+
+
+    }
