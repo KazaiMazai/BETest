6900b76e3f0286c45f7c57be56b0495aefbe2a1c
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-01 15:39:14 +0300

    added env provider

---
+++ BETest/App/ReduxExtensions/EnvironmentProvider.swift
@@ -0,0 +1,22 @@
+//
+//  EnvironmentProvider.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 01.10.2020.
+//
+
+import SwiftUI
+
+struct EnvironmentProvider<Content: View>: View {
+    let store: EnvironmentStore
+    let content: () -> Content
+    var body: some View {
+        content().injectEnvironment(store: store)
+    }
+}
+
+private extension View {
+    func injectEnvironment(store: EnvironmentStore) -> some View {
+        self.environmentObject(store)
+    }
+}
