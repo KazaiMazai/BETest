80ec4f85cc4a1022c3055d9e744d1fb64b4aa0f1
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-02 18:23:37 +0300

    implemented TTS operator

---
+++ BETest/App/Operators/TTSOperator/TextToSpeechRequest.swift
@@ -0,0 +1,37 @@
+//
+//  TextToSpeechRequest.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 02.10.2020.
+//
+
+import Foundation
+
+struct TextToSpeechRequest {
+    internal init(id: UUID,
+                  text: String,
+                  start: @escaping Command,
+                  finish: @escaping Command,
+                  pause: @escaping Command,
+                  cancel: @escaping Command,
+                  continued: @escaping Command,
+                  willStart: @escaping Command) {
+        self.id = id
+        self.text = text
+        self.start = start
+        self.finish = finish
+        self.pause = pause
+        self.cancel = cancel
+        self.continued = continued
+        self.willStart = willStart
+    }
+
+    let id: UUID
+    let text: String
+    let start: Command
+    let finish: Command
+    let pause: Command
+    let cancel: Command
+    let continued: Command
+    let willStart: Command
+}
