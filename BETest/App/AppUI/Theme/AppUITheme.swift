a150b53885eae8392eed9feeae4e112d4df9af99
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-02 13:32:27 +0300

    added baloon styles to app theme

--- BETest/App/AppUI/Theme/AppUITheme.swift
+++ BETest/App/AppUI/Theme/AppUITheme.swift
@@ -21,7 +21,38 @@ struct AppUITheme {
 
 
 extension AppUITheme {
+    struct DialogueView {
+        let background = Color(hex: "#F9FAFB")
+    }
+
     struct BaloonStyle {
-        let font = Font.system(size: 17, weight: .light, design: Font.Design.serif)
+        let font = Font.custom("SFProText-Light", size: 17)
+        let textColor = Color(hex: "#000000")
+        let background = Color(hex: "#FDFDFE")
+        let borderRadius: CGFloat = 5
+        let shadow = Shadow()
+        let paddings = Paddings()
+    }
+}
+
+
+extension AppUITheme.BaloonStyle {
+
+
+    struct Shadow {
+        let color = Color(hex: "#000000")
+        let alpha: CGFloat = 0.5
+        let offset = CGSize(width: 1, height: 1)
+        let blur: CGFloat = 4
+    }
+}
+
+extension AppUITheme.BaloonStyle {
+    struct Paddings {
+        let interItemSpacing = 30
+        let textPaddings = 10
+        let leftContentInset = 20
+        let leftBodyInset = 11
+        let triangeSize = CGSize(width: 11, height: 18)
     }
 }
---
+++ BETest/App/AppUI/UIComponents/BallonView.swift
@@ -0,0 +1,20 @@
+//
+//  BallonView.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 02.10.2020.
+//
+
+import SwiftUI
+
+struct BallonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BallonView_Previews: PreviewProvider {
+    static var previews: some View {
+        BallonView()
+    }
+}
