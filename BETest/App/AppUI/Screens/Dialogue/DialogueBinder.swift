5cbafa4bcbdd5a6120da2cd7ccef3ab19628fce3
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-02 16:34:07 +0300

    implemented dialogue view binder

---
+++ BETest/App/AppUI/Screens/Dialogue/DialogueBinder.swift
@@ -0,0 +1,32 @@
+//
+//  DialogueBinder.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 02.10.2020.
+//
+
+import SwiftUI
+
+struct DialogueBinder: UIBinder {
+    func contentWith(props: DialogueView.Props) -> DialogueView {
+        DialogueView(props: props)
+    }
+
+    func prepareProps(state: AppState, store: EnvironmentStore) -> DialogueView.Props {
+        .init(items: state.prompter.items.map { .init(with: $0) },
+              animationDuration: CGFloat(state.prompter.delay))
+    }
+}
+
+struct DialogueBinder_Previews: PreviewProvider {
+    static var previews: some View {
+        DialogueBinder()
+    }
+}
+
+extension BallonView.Props {
+    init(with model: TextData) {
+        self.init(id: model.id,
+                  text: model.text)
+    }
+}
