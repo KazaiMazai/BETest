fe4e025f3ff6852f1e9580a5722c6d0944c3745e
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-02 19:07:00 +0300

    updated test text data

--- BETest/App/AppSideEffects/FileDataSource.swift
+++ BETest/App/AppSideEffects/FileDataSource.swift
@@ -32,7 +32,7 @@ extension FileDataSource {
             return
         }
 
-        let data = Array(0...10).map { TextData(id: $0, text: "\($0): Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum")}
+        let data = Array(0...10).map { TextData(id: $0, text: "\($0): 100")}
 
         store.dispatch(action: Actions.TextDataSource.ReceievedDataSuccess(value: data))
     }
