1cd261001091e3b4b6f3d023c298a10b548d58d0
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-02 22:49:07 +0300

    refactoring

--- BETest/Core/Components/Dialogue.swift
+++ BETest/Core/Components/Dialogue.swift
@@ -28,51 +28,14 @@ struct Dialogue {
         case is Actions.DialogueFlow.Run:
             state = .waitingForData
         case let action as Actions.TextDataSource.ReceievedDataSuccess:
-            pendingItems.append(contentsOf: action.value)
-            guard waitingForData && !pendingItems.isEmpty else {
-                break
-            }
-
-            let item = pendingItems.removeFirst()
-            state = .processingItem(PayloadRequest(id: UUID(), payload: item),
-                                    speechAvailableAfter: Date().addingTimeInterval(animationsDelay + delay))
-            items.append(item)
+            handleReceiveDataSuccess(action)
         case let action as Actions.SpeechSynthesizer.StateChange:
-            guard case .finish = action.state else {
-                break
-            }
-
-            guard !pendingItems.isEmpty else {
-                break
-            }
-
-            let item = pendingItems.removeFirst()
-            state = .pendingItem(item, availableAfter: Date().addingTimeInterval(delay))
+            handleSynthesizerStateChange(action)
         case let action as Actions.Time.TimeChanged:
-            guard case let .pendingItem(item, availableAfter) = state else {
-                break
-            }
-
-            guard availableAfter <= action.timestamp else {
-                break
-            }
-
-            state = .processingItem(PayloadRequest(id: UUID(), payload: item),
-                                    speechAvailableAfter: Date().addingTimeInterval(animationsDelay + delay))
-            items.append(item)
+            handleTimeChange(action)
         default:
             break
         }
-    }
-}
-
-extension Dialogue {
-    enum State {
-        case none
-        case waitingForData
-        case pendingItem(TextData, availableAfter: Date)
-        case processingItem(PayloadRequest<TextData>, speechAvailableAfter: Date)
-        case finished
     }
 }
 
@@ -95,5 +58,56 @@ extension Dialogue {
         }
 
         return item
+    }
+}
+
+private extension Dialogue {
+    mutating func handleReceiveDataSuccess(_ action: Actions.TextDataSource.ReceievedDataSuccess) {
+        pendingItems.append(contentsOf: action.value)
+        guard waitingForData && !pendingItems.isEmpty else {
+            return
+        }
+
+        let item = pendingItems.removeFirst()
+        state = .processingItem(PayloadRequest(id: UUID(), payload: item),
+                                speechAvailableAfter: Date().addingTimeInterval(animationsDelay + delay))
+        items.append(item)
+    }
+
+    mutating func handleSynthesizerStateChange(_ action: Actions.SpeechSynthesizer.StateChange) {
+        guard case .finish = action.state else {
+            return
+        }
+
+        guard !pendingItems.isEmpty else {
+            return
+        }
+
+        let item = pendingItems.removeFirst()
+        state = .pendingItem(item, availableAfter: Date().addingTimeInterval(delay))
+    }
+
+    mutating func handleTimeChange(_ action: Actions.Time.TimeChanged) {
+        guard case let .pendingItem(item, availableAfter) = state else {
+            return
+        }
+
+        guard availableAfter <= action.timestamp else {
+            return
+        }
+
+        state = .processingItem(PayloadRequest(id: UUID(), payload: item),
+                                speechAvailableAfter: Date().addingTimeInterval(animationsDelay + delay))
+        items.append(item)
+    }
+}
+
+private extension Dialogue {
+    enum State {
+        case none
+        case waitingForData
+        case pendingItem(TextData, availableAfter: Date)
+        case processingItem(PayloadRequest<TextData>, speechAvailableAfter: Date)
+        case finished
     }
 }
