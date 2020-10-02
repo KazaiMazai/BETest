fca5c8a9e71222357523252af8886edf539dffc9
Sergey Kazakov <kazaimazai@gmail.com>
2020-10-02 16:23:28 +0300

    implemented main dialogue view

---
+++ BETest/App/AppUI/Screens/Dialogue/DialogueView.swift
@@ -0,0 +1,50 @@
+//
+//  DialogueView.swift
+//  BETest
+//
+//  Created by Sergey Kazakov on 02.10.2020.
+//
+
+import SwiftUI
+
+extension DialogueView {
+    struct Props {
+        let items: [BallonView.Props]
+        let animationDuration: CGFloat
+
+        static func preview(count: Int) -> Props {
+            .init(
+                items: Array(0..<count).map { .preview(id: $0) },
+                animationDuration: 0.5)
+        }
+    }
+}
+
+struct DialogueView: View {
+    @Environment(\.appUITheme) var theme
+
+    let props: Props
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: theme.baloonStyle.paddings.interItemSpacing) {
+                Color.clear.frame(height: 0)
+
+                ForEach(props.items.reversed()) {
+                    BallonView(props: $0).rotationEffect(.radians(.pi))
+                }
+
+
+            }
+        }
+        .rotationEffect(.radians(.pi))
+        .ignoresSafeArea(edges: .vertical)
+        .background(theme.dialogueViewStyle.background)
+    }
+}
+
+struct DialogueView_Previews: PreviewProvider {
+    static var previews: some View {
+        DialogueView(props: .preview(count: 1))
+    }
+}
